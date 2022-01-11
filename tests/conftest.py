036adb364efd85c24c606e0b7cdade23bfb2af45
kcg <kimchelgi03@gmail.com>
2022-01-11 17:53:28 +0900

    Chore: pytest 샘플 코드 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,6 @@ from fastapi.testclient import TestClient
 from app.main import create_app
 
 
-"""
-1. DB 생성
-2. 테이블 생성
-3. 테스트 코드 작동
-4. 테이블 레코드 삭제 
-"""
-
 @pytest.fixture(scope="session")
 def app():
     os.environ["API_ENV"] = "test"
---
+++ tests/test_response.py
@@ -0,0 +1,10 @@
+import json
+
+def test_main_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_main_contents(client):
+    response = client.get("/services/main_contents?keyword=test")
+    assert response.status_code == 200
+    assert json.loads(response.text)['msg'] == "응답 성공"
