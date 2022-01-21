83102f87aa288c365605ea0bfaa947f607a32ba3
leeyongju <dydwn5390@mqnic.com>
2022-01-21 17:19:11 +0900

    Feat: 유저 정보 반환 API 개발

--- app/database/schema.py
+++ app/database/schema.py
@@ -2,12 +2,67 @@
 from sqlalchemy import (
     Column,
     String,
+    func
 )
 
-from app.database.conn import Base
+from sqlalchemy.orm import Session, relationship
+from app.database.conn import Base, db
+
+class UserRepository:
+    def __init__(self):
+        self._q = None
+        self._session = None
+        self.served = None
+
+    @classmethod
+    def create(cls, session: Session, auto_commit=False, **kwargs):
+        """
+        테이블 데이터 적재 전용 함수
+        :param session:
+        :param auto_commit: 자동 커밋 여부
+        :param kwargs: 적재 할 데이터
+        :return:
+        """
+        obj = cls()
+        for col in obj.all_columns():
+            col_name = col.name
+            if col_name in kwargs:
+                setattr(obj, col_name, kwargs.get(col_name))
+        session.add(obj)
+        session.flush()
+        if auto_commit:
+            session.commit()
+        return obj
+
+    @classmethod
+    def get(cls, session: Session = None, **kwargs):
+        """
+        Simply get a Row
+        :param session:
+        :param kwargs:
+        :return:
+        """
+        sess = next(db.session()) if not session else session
+        query = sess.query(cls)
+        for key, val in kwargs.items():
+            col = getattr(cls, key)
+            query = query.filter(col == val)
+
+        if query.count() > 1:
+            raise Exception("Only one row is supposed to be returned, but got more than one.")
+        result = query.first()
+        if not session:
+            sess.close()
+        return result
+
+    @classmethod
+    def count_users_by_user_id(self, session: Session = None, user_id = ''):
+        sess = next(db.session()) if not session else session
+        result = sess.query(Users).filter(Users.user_id == user_id).count()
+        return result
 
 
-class Users(Base):
+class Users(Base, UserRepository):
     __tablename__ = "Users"
     user_id = Column(String(length=100), primary_key=True, nullable=False)
     user_name = Column(String(length=20), nullable=False)
