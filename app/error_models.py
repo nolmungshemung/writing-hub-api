442424f68abe266e1a2dd12965a816c4a92d1f34
leeyongju <dydwn5390@mqnic.com>
2022-02-03 16:25:41 +0900

    Chore: 필명 등록 에러 스키마 정의

--- app/error_models.py
+++ app/error_models.py
@@ -19,3 +19,13 @@ class NotFoundUserModel(APIExceptionModel):
 
     msg: str = '해당 유저를 찾을 수 없습니다'
     data: Dict = {}
+
+
+class DuplicateNameModel(APIExceptionModel):
+    '''
+    error response schema
+    중복되는 필명일 경우
+    '''
+
+    msg: str = '이미 존재하는 필명입니다.'
+    data: Dict = {}
