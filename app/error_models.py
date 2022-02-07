ecd517d681d571c920521145e48a865e13dfe1d8
iceblend <mildroast@naver.com>
2022-02-08 00:34:38 +0900

    Feat: 에러 raise 수정 및 에러 모델 수정

--- app/error_models.py
+++ app/error_models.py
@@ -22,6 +22,49 @@ class NotFoundUserModel(APIExceptionModel):
     data: Dict = {}
 
 
+class DuplicateNameModel(APIExceptionModel):
+    '''
+    error response schema
+    중복되는 필명일 경우
+    '''
+
+    msg: str = '이미 존재하는 필명입니다.'
+    data: Dict = {}
+
+
+class NotFoundContentModel(APIExceptionModel):
+    '''
+    error response schema
+    해당 식별자의 작품이 없는 경우
+    '''
+
+    msg: str = '해당 작품을 찾을 수 없습니다.'
+    data: Dict = {}
+
+
+class NotOriginalContentModel(APIExceptionModel):
+    '''
+    error response schema
+    해당 식별자의 작품이 없는 경우
+    '''
+
+    msg: str = '원문이 아닙니다.'
+    data: Dict = {}
+
+
+class NotFoundFeedContentModel(APIExceptionModel):
+    '''
+    error response schema
+    해당 식별자의 작품이 없는 경우
+    '''
+
+    msg: str = '작가의 작품을 찾을 수 없습니다.'
+    data: Dict = {}
+
+
+
+
+
 class NotProperWritingContents(WritingContents):
     '''
     error response schema
